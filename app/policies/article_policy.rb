aa4768c9ab26b54b9f9e5b838fe810b14e1c701d
Palamariuk <palamariuk.maksym@gmail.com>
2021-05-23 02:11:02 +0300

    Add policy throught pundit

---
+++ app/policies/article_policy.rb
@@ -0,0 +1,25 @@
+class ArticlePolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    author?
+  end
+
+  def destroy?
+    author?
+  end
+
+  private
+
+  def author?
+    return false unless user
+
+    record.author_id == user.id
+  end
+end
